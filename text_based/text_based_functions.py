75426174ba2f79cf1d2f79783c7d0eb86f762971
NuMosaic <melisofone@gmail.com>
2021-12-09 00:30:08 -0500

    adding to marker processing

--- text_based/text_based_functions.py
+++ text_based/text_based_functions.py
@@ -38,7 +38,7 @@ class Character():
 
     def __init__(self, 
             name : str = "Bob", 
-            speeds : list = [0.1, 0.5, 0.8]) -> None:
+            speeds : tuple = (0.1, 0.5, 0.8)) -> None:
         self.name = name
         self._speech_speed = speeds[0]
         self._soft_punctuation_speed = speeds[1]
@@ -102,6 +102,36 @@ def display_text(text: str,
     return
 
 
+def narrate_story(text: str,
+        speed_level: int) -> int:
+    """
+    Parameters:
+        text: string
+            A string to be written to the console.
+        speed_level: int
+            Signifier of the display levels. 0 means 
+            slow, 1 means medium, and 2 means fast.
+
+    Returns:
+         : int
+            Signifies if the function ran successfully. 
+            0 means success, 1 means fail.
+    """
+    global Narrator
+    speed_list = []
+    if speed_level == 0:
+        speed_list = [0.16, 0.5, 0.8]
+    elif speed_level == 1:
+        speed_list = [0.08, 0.25, 0.5]
+    elif speed_level == 2:
+        speed_list = [0.04, 0.15, 0.25]
+    else:
+        return 1
+    Nar = Character("Narrator", tuple(speed_list))
+    display_text(text, Nar)
+    return 0
+
+
 def read_input():
     for line in Reader:
         if line.strip() == 'q':
@@ -132,51 +162,97 @@ def load_story_script(filepath: str) -> list[str]:
     return file_lines
 
 
-def _read_branch_marker():
-    print("branch.")
+def parse_branch_marker(line: str) -> dict:
+    """
+    Parameters:
+        line: string
+            The line with the branches marker to be 
+            parsed.
+
+    Returns:
+        branches_dict: dict
+            A dictionary of the choices (string) and 
+            their corresponding line to jump to (int) 
+            if chosen.
+    """
+    branches_dict = {}
+    branches_list = line.strip()[:-1].split(" ")[1:]
+    for opt in branches_list:
+        branches = opt.split("-")
+        branches_dict[branches[0]] = int(branches[1])
+    return branches_dict
+
+
+def end_message():
+    """
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    print("\t\t\t~~~ THE END! ~~~")
     return
 
 
-def _read_end_marker():
-    print("end.")
-    return
+def read_markers(line: str,
+        speed_level: int) -> int:
+    """
+    Parameters:
+        line: string
+            The line from the story file to be 
+            analyzed for markers.
+        speed_level: int
+            Signifier of the display levels. 0 means 
+            slow, 1 means medium, and 2 means fast.
 
-
-def _handle_crossroads_content():
-    print("crossroad content.")
-    return
-
-
-def _handle_story_content():
-    print("story.")
-    return
-
-
-def read_markers(line: str):
+    Returns:
+        status: int
+            signifies whether the function ran properly. 
+            0 means success, 1 means fail, 2 means 
+            end the game, 3 means parse branch, 4 
+            means check for player input, and 5 means 
+            narrate.
+    """
     global markers, crossroad
     line = line.strip()
-    if line == markers["end"]:
-        _read_end_marker()
-    elif re.match(markers["branch"], line):
-        _read_branch_marker()
+    if line == markers["end"]: # end game
+        return 2
+    elif re.match(markers["branch"], line): # parse branch options
+        return 3
     elif line == markers["crossroads"][0]:
-        print("crossroad start ->")
         crossroad = True
-    elif line == markers["crossroads"][1]:
-        print("<- crossroad end")
+    elif line == markers["crossroads"][1]: # check for player input
         crossroad = False
-    else:
-        if crossroad is True:
-            _handle_crossroads_content()
-        else:
-            _handle_story_content()
+        return 4
+    else: # narrate
+        return 5
+    return 0
+
+
+def read_player_input() -> str:
+    """
+    """
     return
 
 
 # TESTING
 if __name__ == "__main__":
-    #John = Character("John")
     a = load_story_script("test3.txt")
-    for l in a:
-        #display_text(l, John)
-        read_markers(l)
+    options = {}
+    start, status = 0, 0
+    speed_level = 0.0
+    player_choice = ''
+
+    for l in range(start, len(a)):
+        s = read_markers(a[l], 1)
+        if s == 2:
+            end_message()
+            break
+        elif s == 3:
+            options = parse_branch_marker(a[l])
+            # go to line player_choice corresponds to in options
+        elif s == 4:
+            player_choice = read_player_input()
+        elif s == 5:
+            status = narrate_story(a[l], speed_level)
